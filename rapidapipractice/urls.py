dbdd3d07cb56ef970357bcdfa77b44a6103801f7
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-10-14 20:27:39 +0800

    filenames

--- rapidapipractice/urls.py
+++ rapidapipractice/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from django-practice.api import views
+import djangoAPI.api.views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -26,4 +26,4 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+]]
