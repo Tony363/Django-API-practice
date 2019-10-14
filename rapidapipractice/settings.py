b2d5ccd091e31c28beef22984d007471f3274949
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-10-14 20:57:08 +0800

    playing around

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2@#tk5^vem)rv1+-in=32-5yb3pbcbjl+m30o)6=3!qdpq5mv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
