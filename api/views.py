8ec47760d08e06d8ba29917646aa0725a4b4cdc7
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-10-14 20:42:17 +0800

    importing

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-From .serializers import UserSerializer, GroupSerializer
+from rapidapipractice.serializers import UserSerializer, GroupSerializer
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
--- rapidapipractice/urls.py
+++ rapidapipractice/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-import djangoAPI.api.views
+from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
